31715607a49d3a8ae9e47521af5fefeab8474209
José Martínez <jgermanmm@gmail.com>
2026-03-03 21:43:57 -0300

    logout personalizado

--- auth_ejemplo/urls.py
+++ auth_ejemplo/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import dashboard, home
+from .views import dashboard, home, custom_logout
 
 urlpatterns = [
     path('', home, name='home'),
     path('dashboard/', dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', custom_logout, name='logout')
 ]
--- auth_ejemplo/views.py
+++ auth_ejemplo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -11,3 +14,9 @@ def home(request):
 @login_required
 def dashboard(request):
     return render(request, 'auth/dashboard.html')
+
+
+def custom_logout(request):
+    logout(request)
+    messages.success(request, "Sesión cerrada correctamente.")
+    return redirect('home')
